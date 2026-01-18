cec05ddb8c720b5c24faf6363d066a4305026932
Abhijeet Rav <abhijeetrav56@gmail.com>
2026-01-18 22:59:12 +0530

    first commit

---
+++ test/test__operations.py
@@ -0,0 +1,11 @@
+from src .math_operations import add_numbers, sub_numbers
+
+def test_add_numbers():
+    assert add_numbers(2, 3) == 5
+    assert add_numbers(-1, 1) == 0
+    assert add_numbers(0, 0) == 0
+
+def test_sub_numbers():
+    assert sub_numbers(5, 3) == 2
+    assert sub_numbers(1, 1) == 0
+    assert sub_numbers(0, 0) == 0
